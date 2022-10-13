e5100c6cbc0bca8cec5dd9098bb1071c58213572
MikeNotu <michael.bateman@globant.com>
2022-10-13 13:54:20 -0500

    Finished First version

--- src/components/Leet0001TwoSum.js
+++ src/components/Leet0001TwoSum.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "../styles/Leet0001TwoSum.css";
 
 function Leet0001TwoSum() {
   // const nums = [3, 2, 4];
@@ -17,15 +18,13 @@ function Leet0001TwoSum() {
   //     }
   //   }
   // };
-
+  const [nums] = useState([3, 2, 4]);
+  const target = 6;
   const [counter, setCounter] = useState(0);
   const [previousValues] = useState({});
   const [result, setResult] = useState(null);
 
   useEffect(() => {
-    const nums = [3, 2, 4];
-    const target = 6;
-
     result === null &&
       setTimeout(() => {
         const neededValue = target - nums[counter];
@@ -46,21 +45,20 @@ function Leet0001TwoSum() {
         }
         setCounter(counter + 1);
       }, 1000);
-  }, [counter, result, previousValues]);
+  }, [counter, result, previousValues, nums]);
 
   return (
     <div>
       <h1>1. Two Sum</h1>
-      <h4>
+      <h4 className="description">
         Given an array of integers nums and an integer target, return indices of
         the two numbers such that they add up to target. You may assume that
         each input would have exactly one solution, and you may not use the same
         element twice. You can return the answer in any order.
       </h4>
-      <p>Input: nums = [2,7,11,15]</p>
-      <p>target = 9 Output: [0,1]</p>
-      <p>Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].</p>
-      <p>{counter}</p>
+      {/* <p>Input: nums = {JSON.stringify(nums)}</p>
+      <p>target = {JSON.stringify(target)}</p> */}
+      <p>Number of Iterations: {counter}</p>
       <p>{JSON.stringify(previousValues)}</p>
       <p>
         {result !== null
